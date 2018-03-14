3cf37b6c0d2d8181ded27f28f49365f52f6086dd
BKertesz <kertesz.balazs92@gmail.com>
2018-03-14 14:20:16 +0000

    Improved game file

--- main.rb
+++ main.rb
@@ -3,6 +3,33 @@ require_relative "player"
 require_relative "hiddenword"
 
 clear_code = %x{clear}
-p clear_code
+def setup_player()
+  print "Hello! What should be your name?"
+  player_name = gets.chomp
+  print "How many round would you like to play?"
+  lives = gets.chomp
+  lives = lives.to_i
+  player = Player.new(player_name,lives)
+end
+
+def setup_hidden_word()
+  print "What should be the hidden word?"
+  word = gets.chomp
+  hidden_word = Hiddenword.new(word)
+  print clear_code
+end
+
+def setup_game()
+  game = Game.new(player,hidden_word)
+end
+
+def run_game_loop()
+  while game.is_won? && game.is_lost?
+
+  end
+
+end
+
+
 
 # EOF
