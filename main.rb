d9b426c52cbe76d5676840438e07e4e1009ffe0c
BKertesz <kertesz.balazs92@gmail.com>
2018-03-14 14:39:43 +0000

    First stable version

--- main.rb
+++ main.rb
@@ -2,34 +2,52 @@ require_relative "game"
 require_relative "player"
 require_relative "hiddenword"
 
-clear_code = %x{clear}
-def setup_player()
-  print "Hello! What should be your name?"
-  player_name = gets.chomp
-  print "How many round would you like to play?"
-  lives = gets.chomp
-  lives = lives.to_i
-  player = Player.new(player_name,lives)
-end
-
-def setup_hidden_word()
-  print "What should be the hidden word?"
-  word = gets.chomp
-  hidden_word = Hiddenword.new(word)
-  print clear_code
-end
-
-def setup_game()
-  game = Game.new(player,hidden_word)
-end
-
-def run_game_loop()
-  while game.is_won? && game.is_lost?
-
+class GameLoop
+  def clear_screen
+    print %x{clear}
   end
 
+  def setup_player()
+    puts "Hello! What should be your name?"
+    player_name = gets.chomp
+    puts "How many round would you like to play?"
+    lives = gets.chomp
+    lives = lives.to_i
+    @player = Player.new(player_name,lives)
+  end
+
+  def setup_hidden_word()
+    puts "What should be the hidden word?"
+    word = gets.chomp
+    @hidden_word = Hiddenword.new(word)
+    clear_screen()
+  end
+
+  def setup_game()
+    @game = Game.new(@player,@hidden_word)
+  end
+
+  def run_game_loop()
+    until @game.is_won? || @game.is_lost?
+      puts "Lifes left:#{@game.player.lifes_left}"
+      puts "Guessed letters:#{@game.guessed_letters}"
+      puts "The word: #{@game.hidden_word.display}"
+      puts ("Guess a letter:")
+      guess = gets.chomp
+      @game.guess_a_letter(guess)
+    end
+    puts "You won #{@game.player.name}!" if @game.is_won?
+    puts "You lost #{@game.player.name}, the word was #{@game.hidden_word.word}" if @game.is_lost?
+  end
 end
 
+window = GameLoop.new()
+window.clear_screen()
+window.setup_player()
+window.setup_hidden_word()
+window.setup_game()
+window.run_game_loop()
 
 
-# EOF
+
+  # EOF
